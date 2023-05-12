1402b2e2b70852f84d2f376ba5ce253ac2587518
Iqbalfebtien <iqbalfebtien@gmail.com>
2023-05-13 02:52:16 +0700

    Initial commit

---
+++ app/src/main/java/com/iqbal/WisataMalang/CandiJago.java
@@ -0,0 +1,14 @@
+package com.iqbal.WisataMalang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CandiJago extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_candi_jago);
+    }
+}
---
+++ app/src/main/java/com/iqbal/WisataMalang/greenpark.java
@@ -0,0 +1,14 @@
+package com.iqbal.WisataMalang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class greenpark extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_green_park);
+    }
+}
